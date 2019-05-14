e56ff0c35e8dbec982662a6527e6af967e886ef7
Adam Filiz <afiliz@ucsc.edu>
2019-05-14 01:04:52 -0700

    Fixed some build issue stuff, added garbo data

--- app/src/main/java/ucsc121/carboncars/MonthlyActivity.java
+++ app/src/main/java/ucsc121/carboncars/MonthlyActivity.java
@@ -2,12 +2,59 @@ package ucsc121.carboncars;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class MonthlyActivity extends AppCompatActivity {
+
+    BarChart monthlyBarChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly);
+
+        monthlyBarChart = findViewById(R.id.monthlyBarChart);
+
+        ArrayList NoOfEmp = new ArrayList();
+
+        NoOfEmp.add(new BarEntry(945f, 0));
+        NoOfEmp.add(new BarEntry(1040f, 1));
+        NoOfEmp.add(new BarEntry(1133f, 2));
+        NoOfEmp.add(new BarEntry(1240f, 3));
+        NoOfEmp.add(new BarEntry(1369f, 4));
+        NoOfEmp.add(new BarEntry(1487f, 5));
+        NoOfEmp.add(new BarEntry(1501f, 6));
+        NoOfEmp.add(new BarEntry(1645f, 7));
+        NoOfEmp.add(new BarEntry(1578f, 8));
+        NoOfEmp.add(new BarEntry(1695f, 9));
+
+        ArrayList year = new ArrayList();
+
+        year.add("2008");
+        year.add("2009");
+        year.add("2010");
+        year.add("2011");
+        year.add("2012");
+        year.add("2013");
+        year.add("2014");
+        year.add("2015");
+        year.add("2016");
+        year.add("2017");
+
+        BarDataSet bardataset = new BarDataSet(NoOfEmp, "No Of Employee");
+        BarDataSet bardataset2 = new BarDataSet(year, "Years");
+        monthlyBarChart.animateY(5000);
+        BarData data = new BarData(bardataset2, bardataset);
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        monthlyBarChart.setData(data);
     }
 }
