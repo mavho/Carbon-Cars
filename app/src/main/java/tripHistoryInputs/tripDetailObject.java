42e4ffed3a3811689b7175b741ac15f0d89e8a8f
Abdul <awahmed@ucsc.edu>
2019-05-14 15:54:04 -0700

    trip history helper classes

---
+++ app/src/main/java/tripHistoryInputs/tripDetailObject.java
@@ -0,0 +1,16 @@
+package tripHistoryInputs;
+
+public class tripDetailObject {
+    private String rideID;
+
+    public tripDetailObject(String rideid){
+        this.rideID = rideid;
+
+    }
+    public String getRideID(){
+        return rideID;
+    }
+
+
+
+}
