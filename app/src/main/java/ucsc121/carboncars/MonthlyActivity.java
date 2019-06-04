ed4e0cc7f7c5b07e9737a5b1bbb22cd184cddcac
Adam Filiz <afiliz@ucsc.edu>
2019-06-04 03:33:52 -0700

    Adding progress for dataviz

--- app/src/main/java/ucsc121/carboncars/MonthlyActivity.java
+++ app/src/main/java/ucsc121/carboncars/MonthlyActivity.java
@@ -1,8 +1,10 @@
 package ucsc121.carboncars;
 
-import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
@@ -16,20 +18,22 @@ import com.anychart.enums.Anchor;
 import com.anychart.enums.MarkerType;
 import com.anychart.enums.TooltipPositionMode;
 import com.anychart.graphics.vector.Stroke;
+import android.database.Cursor;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import java.util.Random;
 
-public class WeeklyActivity extends AppCompatActivity {
+// AnyCharts code here based off code from the AnyCharts
+// sample code in https://github.com/AnyChart/AnyChart-Android
+
+public class MonthlyActivity extends AppCompatActivity {
 
     DataBase carbonDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weekly);
+        setContentView(R.layout.activity_monthly);
 
         AnyChartView anyChartView = findViewById(R.id.any_chart_view);
         anyChartView.setProgressBar(findViewById(R.id.progress_bar));
@@ -47,7 +51,7 @@ public class WeeklyActivity extends AppCompatActivity {
 
         cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
 
-        cartesian.title("Pounds of CO2 Emitted in the Past Week");
+        cartesian.title("Pounds of CO2 Emitted in the Past Year");
 
         cartesian.yAxis(0).title("Pounds of CO2 Emitted");
         cartesian.xAxis(0).labels().padding(2d, 0d, 2d, 0d);
@@ -57,27 +61,39 @@ public class WeeklyActivity extends AppCompatActivity {
 
         List<DataEntry> seriesData = new ArrayList<>();
 
-        int cursorTracker = 0;
-        while(trips.moveToNext()){
-            if(cursorTracker > 7) break;
+        int cursorCounter = 0;
+        for(int i = 0; i < 30; i++){
+            String date = trips.getString(2); // get date
+            Double co2 = trips.getDouble(4); // get co2
+            String nums[] = date.split("/");
+            Log.d("MonthlyActivity", "Day: + " + nums[0]);
+            if(nums[0].charAt(0) == '0') nums[0] = nums[0].substring(1); // check if theres a 0 in day string
+            int day = Integer.parseInt(nums[0]); // turn day into int
+            if(cursorCounter == 0) cursorCounter = day; // initialize cursor counter to first day
 
-            String date = trips.getString(2);
-            Double co2 = trips.getDouble(4);
-            String day = getDay(date);
+            if(day - 1 != cursorCounter){
+                day = cursorCounter + 1;
+                if(cursorCounter != 31) cursorCounter = cursorCounter % 31;
+                co2 = 0.0;
+            }
+            cursorCounter = cursorCounter + 1;
 
-            seriesData.add(new CustomDataEntry(day, co2));
-
-            cursorTracker++;
+            seriesData.add(new CustomDataEntry(Integer.toString(day), co2));
         }
         trips.close();
 
-        seriesData.add(new CustomDataEntry("Mon", 3.6));
-        seriesData.add(new CustomDataEntry("Tue", 5.6));
-        seriesData.add(new CustomDataEntry("Wed", 8.6));
-        seriesData.add(new CustomDataEntry("Thu", 10.6));
-        seriesData.add(new CustomDataEntry("Fri", 4.6));
-        seriesData.add(new CustomDataEntry("Sat", 1.6));
-        seriesData.add(new CustomDataEntry("Sun", 2.6));
+        seriesData.add(new CustomDataEntry("Jan", 3.6));
+        seriesData.add(new CustomDataEntry("Feb", 5.6));
+        seriesData.add(new CustomDataEntry("Mar", 8.6));
+        seriesData.add(new CustomDataEntry("Apr", 10.6));
+        seriesData.add(new CustomDataEntry("May", 4.6));
+        seriesData.add(new CustomDataEntry("Jun", 1.6));
+        seriesData.add(new CustomDataEntry("Jul", 2.6));
+        seriesData.add(new CustomDataEntry("Aug", 7.6));
+        seriesData.add(new CustomDataEntry("Sep", 6.6));
+        seriesData.add(new CustomDataEntry("Oct", 8.6));
+        seriesData.add(new CustomDataEntry("Nov", 9.6));
+        seriesData.add(new CustomDataEntry("Dec", 11.6));
 
         Set set = Set.instantiate();
         set.data(seriesData);
@@ -99,41 +115,47 @@ public class WeeklyActivity extends AppCompatActivity {
         anyChartView.setChart(cartesian);
     }
 
-    public String getDay(String date){
+    public String getMonth(String date){
         String nums[] = date.split("/");
-        int day = Integer.parseInt(nums[1]);
         int month = Integer.parseInt(nums[0]);
-        int year = Integer.parseInt(nums[2]);
-
-        Calendar myCal = Calendar.getInstance();
-        myCal.set(Calendar.DAY_OF_MONTH, day);
-        myCal.set(Calendar.MONTH, month);
-        myCal.set(Calendar.YEAR, year);
-        int dayOfWeek = myCal.get(Calendar.DAY_OF_WEEK);
-
         String result = "";
 
-        switch(dayOfWeek){
+        switch(month){
             case 1:
-                result = "Sun";
+                result = "Jan";
                 break;
             case 2:
-                result = "Mon";
+                result = "Feb";
                 break;
             case 3:
-                result = "Tue";
+                result = "Mar";
                 break;
             case 4:
-                result = "Wed";
+                result = "Apr";
                 break;
             case 5:
-                result = "Thu";
+                result = "May";
                 break;
             case 6:
-                result = "Fri";
+                result = "Jun";
                 break;
             case 7:
-                result = "Sat";
+                result = "Jul";
+                break;
+            case 8:
+                result = "Aug";
+                break;
+            case 9:
+                result = "Sep";
+                break;
+            case 10:
+                result = "Oct";
+                break;
+            case 11:
+                result = "Nov";
+                break;
+            case 12:
+                result = "Dec";
                 break;
         }
 
@@ -147,5 +169,4 @@ public class WeeklyActivity extends AppCompatActivity {
         }
 
     }
-
 }
