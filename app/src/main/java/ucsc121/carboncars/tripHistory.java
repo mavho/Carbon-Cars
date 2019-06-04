dc9869945eac06846476fb6134d500eca93ea625
mavHo <ho.maverick@gmail.com>
2019-06-04 00:39:10 -0700

    Button to circle, dynamic onclick listener, units to trip history list

--- app/src/main/java/ucsc121/carboncars/tripHistory.java
+++ app/src/main/java/ucsc121/carboncars/tripHistory.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.LayoutManager;
 import android.util.Log;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import android.database.sqlite.SQLiteDatabase;
@@ -43,20 +44,24 @@ public class tripHistory extends AppCompatActivity {
         carbondb.insertTripData(9823.0, "Honda",51.3, "Test4","01/24/19");
 
         Cursor trips = carbondb.getAllTrips();
+        DecimalFormat df = new DecimalFormat("#.###");
         while(trips.moveToNext()){
             //trip,carmodel,date,distance,co2
             String tripNum = trips.getString(0);
             String model = trips.getString(1);
             String date = trips.getString(2);
-            Integer distance = trips.getInt(3);
-            Integer co2 = trips.getInt(4);
+            Double distance = trips.getDouble(3);
+            Double co2 = trips.getDouble(4);
+
             Log.d("trip history","in while loop");
 
+            double tdistance = Double.parseDouble(df.format(distance));
+            double tco2 = Double.parseDouble(df.format(co2));
 
             String i = tripNum;
             String j = date;
-            String k = Integer.toString(distance);
-            String l = Integer.toString(co2);
+            String k = tdistance + " km";
+            String l = tco2 + " lbs of CO2";
             String m = model;
             tripDetailObject cardata1 = new tripDetailObject(i,m,j,k,l);
 //            tripDetailObject cardata2 = new tripDetailObject(j);
@@ -69,40 +74,7 @@ public class tripHistory extends AppCompatActivity {
 //            resultsHistory.add(cardata4);
         }
         trips.close();
-
-
-
-//        String i = "Trip 1                  5 miles         5kg CO2\n";
-//        String j = "Trip 2                  10 miles       12kg CO2\n";
-//        String k = "Trip 3                  20 miles       19kg CO2\n";
-//        String l = "Trip 4                  12 miles       13kg CO2\n";
-//        String m = "Trip 5                  15 miles       14kg CO2\n";
-//        String n = "Trip 6                  4 miles        3kg CO2\n";
-//        String o = "Trip 7                  22 miles       21kg CO2\n";
-//        String p = "Trip 8                  34 miles       29kg CO2\n";
-//        String q = "Trip 9                  51 miles       33kg CO2\n";
-//        tripDetailObject cardata1 = new tripDetailObject(i);
-//        tripDetailObject cardata2 = new tripDetailObject(j);
-//        tripDetailObject cardata3 = new tripDetailObject(k);
-//        tripDetailObject cardata4 = new tripDetailObject(l);
-//        tripDetailObject cardata5 = new tripDetailObject(m);
-//        tripDetailObject cardata6 = new tripDetailObject(n);
-//        tripDetailObject cardata7 = new tripDetailObject(o);
-//        tripDetailObject cardata8 = new tripDetailObject(p);
-//        tripDetailObject cardata9 = new tripDetailObject(q);
-//        resultsHistory.add(cardata1);
-//        resultsHistory.add(cardata2);
-//        resultsHistory.add(cardata3);
-//        resultsHistory.add(cardata4);
-//        resultsHistory.add(cardata5);
-//        resultsHistory.add(cardata6);
-//        resultsHistory.add(cardata7);
-//        resultsHistory.add(cardata8);
-//        resultsHistory.add(cardata9);
         Log.d(TAG, "adding data?");
-
-
-
 
 
         pastTrip.setNestedScrollingEnabled(false);
